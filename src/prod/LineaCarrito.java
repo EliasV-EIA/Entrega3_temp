0dd1cca91ef73306243c36f72064174a8a725a6d
EliasV-EIA <elias.velez@eia.edu.co>
2025-10-20 14:05:42 -0500

    getters y setters

--- src/prod/LineaCarrito.java
+++ src/prod/LineaCarrito.java
@@ -14,4 +14,28 @@ public class LineaCarrito {
     public double calcularSubtotal(){
         return this.producto.getPrecio()*cantidad;
     }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
